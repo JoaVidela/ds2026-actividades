ceb872c13b38425b3d813b7f939c5279cad46546
JoaVidela <joaquin.videla@alu.frlp.utn.edu.ar>
2026-04-19 16:16:40 -0300

    Ejercicio 1 - Variables y Operaciones

---
+++ ejerciciosc03-javascript/ej1-variables/app.js
@@ -0,0 +1,15 @@
+//Declaracion de Constantes
+const nombre  = "Joaquin Videla";
+const edad = "19";
+const materia = "Desarrollo de Software";
+
+// Monstrar mensaje
+console.log (`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+let contador = 0;
+contador = contador + 1;
+contador = contador + 1;
+contador = contador + 1;
+
+//Mostrar valor del contador
+console.log("Valor final del contador:" , contador);
