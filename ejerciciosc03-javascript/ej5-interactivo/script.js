862f112c30e4b419c1fe72815d12341ea51c8bf2
JoaVidela <joaquin.videla@alu.frlp.utn.edu.ar>
2026-04-19 17:49:33 -0300

    Ejercicio 5 - DOM: Input y Boton

---
+++ ejerciciosc03-javascript/ej5-interactivo/script.js
@@ -0,0 +1,33 @@
+function generarAsteriscos(n) {
+    let cadena = "";
+    for (let i = 0; i < n; i++) {
+        cadena += "*";
+    }
+    return cadena;
+}
+
+const inputAltura = document.getElementById('inputAltura');
+const btnGenerar = document.getElementById('btnGenerar');
+const resultadoArbol = document.getElementById('resultadoArbol');
+const errorArbol = document.getElementById('errorArbol');
+
+btnGenerar.addEventListener('click', function() {
+    const altura = parseInt(inputAltura.value);
+    // Limpiar resultados anteriores
+    resultadoArbol.textContent = "" ;
+    errorArbol.textContent = "";
+
+    //Validacion de Altura
+    if (isNaN(altura) || altura < 1) {
+        errorArbol.textContent = 'Por favor, ingresa una altura válida (número entero positivo).';
+        return;
+    }
+
+    //Generacion del arbol
+    let arbol = '';
+    for (let i = 1; i <= altura; i++) {
+        arbol += generarAsteriscos(i) + '\n';
+    }
+    // Mostrar el resultado en el <pre>
+    resultadoArbol.textContent = arbol;
+});
